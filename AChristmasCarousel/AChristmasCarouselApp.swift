b62f9abb6fdfe764ec356d4248fb6e7969deb8ef
Rachel Radford <rachelradford21@gmail.com>
2023-12-16 15:25:08 -0500

    Initial Commit

---
+++ AChristmasCarousel/AChristmasCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  AChristmasCarouselApp.swift
+//  AChristmasCarousel
+//
+//  Created by Rachel Radford on 12/16/23.
+//
+
+import SwiftUI
+
+@main
+struct AChristmasCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
