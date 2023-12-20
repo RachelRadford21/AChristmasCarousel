5d0bd7e3b0cc81f6dab98f759d9cb31dc65c57d9
Rachel Radford <rachelradford21@gmail.com>
2023-12-20 18:55:26 -0500

    changed the image borders in tabview

--- AChristmasCarousel/View/ChristmasImagesScreen/ChristmasImageView.swift
+++ AChristmasCarousel/View/ChristmasImagesScreen/ChristmasImageView.swift
@@ -12,36 +12,23 @@ struct ChristmasImageView: View {
     @Query var images: [Images] = []
     @AppStorage("isDarkMode") private var isDarkMode = false
     var body: some View {
-        DarkModeView()
-        
-        
-        TabView {
-            
-            ForEach(images, id: \.self) { image in
-                
-                AsyncImage(url: URL(string: image.urls.regular)) { urlImage in
-                   RoundedRectangle(cornerRadius: 20)
-                        .foregroundStyle(isDarkMode ? Color.xmasGreen : Color.xmasRed)
-                        .frame(width: 250, height: 250)
-                        .overlay {
-                    urlImage
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 250, height: 250)
-                        .clipShape(RoundedRectangle(cornerRadius: 20))
-                        
-                           
-                        }
-                    
-                    
-                } placeholder: {
-                    ProgressView(String("🎄"))
+        VStack {
+            DarkModeView()
+            TabView {
+                ForEach(images, id: \.self) { image in
+                    AsyncImage(url: URL(string: image.urls.regular)) { urlImage in
+                        urlImage
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                            .scaleEffect(CGSize(width: 1.0, height: 1.0))
+                            .padding(.horizontal, 5)
+                    } placeholder: {
+                        ProgressView(String("🎄"))
+                    }
                 }
             }
-            
+            .tabViewStyle(.page(indexDisplayMode: .never))
         }
-        .tabViewStyle(.page(indexDisplayMode: .never))
-        
-        
     }
 }
